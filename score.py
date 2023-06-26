31a9d297248f278f08b099ecd2622b584fb02482
zuoshifan <zuoshifan@163.com>
2023-06-26 20:23:05 +0800

    Add score computation

---
+++ score.py
@@ -0,0 +1,16 @@
+"""Compute score of the estimated power spectrum, the higer score the better result."""
+
+import numpy as np
+
+
+true_pk2d = np.loadtxt('/home/s1_tianlai/kfyu/SDC3/TestDataset/TestDatasetTRUTH_166MHz-181MHz.data')
+test_pk2d = np.loadtxt('./TianlaiTest_166MHz_181MHz.data')
+test_err = np.loadtxt('./TianlaiTest_166MHz_181MHz_errors.data')
+
+
+def log_prob(pk_true, pk_estimate, pk_error):
+    prob = 1.0 / (np.sqrt(2*np.pi) * pk_error) * np.exp(-(pk_true - pk_estimate)**2 / (2 * pk_error**2))
+    return np.sum(np.log10(prob))
+
+score = log_prob(true_pk2d, test_pk2d, test_err)
+print('score: ', score)
